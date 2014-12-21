b7b494f25d83088830416a95c99afc6dff46175e
montecruiseto <samir.bekaert@gmail.com>
2014-12-21 16:40:17 +0700

    Refactor

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,11 @@ var sass = require('gulp-ruby-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
-// Create an object to define the paths we need
-var paths = {
- html: 'app/index.html',
- build: 'build'
-};
-
 // Setup the default gulp task
-gulp.task('default', function(){
- gulp.src(paths.html)
- .pipe(gulp.dest(paths.build));
+gulp.task('default', function() {
+    gulp.src([
+            'app/index.html',
+            'app/styles/screen.css'
+        ])
+        .pipe(gulp.dest('build'));
 });
