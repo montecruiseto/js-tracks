64d783120d36744e70152764a46d9823919cbb6b
Samirbekaert <samir@myhotelcard.be>
2015-01-02 09:45:49 +0700

    Update 2015-01-02T02:45:49.263Z

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 * result to the HTML element with id "main"
 */
 
-$.getJSON('/../data/tracks.json', function(data) {
+$.getJSON('/tracks.json', function(data) {
     $("#main").html(template.block(data));
 });
 
