0e2ae79f28e90851a15dd01c67b64c14f62d7f94
montecruiseto <samir.bekaert@gmail.com>
2015-01-03 11:13:11 +0700

    Add linenumbers, refactor layout to flexbox

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,9 +1,26 @@
 /*
+* Generate & inject margin line numbers
+*/
+Handlebars.registerHelper('liner', function(context, options) {
+  var out = "", data;
+
+  for (var i=1; i<=(context.length*4); i++) {
+    if (options.data) {
+      data = Handlebars.createFrame(options.data || {});
+      data.index = i;
+    }
+
+    out += "<li>" + options.fn(context[i], { data: data }) + "</li>";
+  }
+
+  return out;
+});
+
+/*
 * Fetch our data, pass it to our pre-compiled template and attach the
-* result to the HTML element with id "main"
+* result to our main container
 */
 
 $.getJSON('data/tracks.json', function(data) {
     $("#main").html(template.block(data));
 });
-
